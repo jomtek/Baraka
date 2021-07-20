ddb9fd283f4f4964e33fd17cc3b5f14e23c23868
Jomtek <jomtek360@yahoo.fr>
2021-07-21 00:29:00 +0200

    Clean workspace

--- Baraka/Theme/UserControls/Quran/Displayer/BarakaVerse.xaml.cs
+++ Baraka/Theme/UserControls/Quran/Displayer/BarakaVerse.xaml.cs
@@ -4,8 +4,10 @@ using Baraka.Data.Surah;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Drawing;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Documents;
 
 namespace Baraka.Theme.UserControls.Quran.Displayer
 {
@@ -21,6 +23,8 @@ namespace Baraka.Theme.UserControls.Quran.Displayer
         {
             InitializeComponent();
 
+            DataContext = this;
+
             Surah = surah;
             Number = number;
         }
@@ -34,12 +38,13 @@ namespace Baraka.Theme.UserControls.Quran.Displayer
 
             if (config.DisplayArabic)
             {
-                ArabicTB.Text = versions["ARABIC"].Verses[verNum];
-                ArabicTB.Visibility = Visibility.Visible;
+                string verse = versions["ARABIC"].Verses[verNum];
+                ArabicRTB_Run.Text = verse;
+                ArabicRTB.Visibility = Visibility.Visible;
             }
             else
             {
-                ArabicTB.Visibility = Visibility.Collapsed;
+                ArabicRTB.Visibility = Visibility.Collapsed;
             }
 
             if (config.DisplayPhonetic)
@@ -78,8 +83,63 @@ namespace Baraka.Theme.UserControls.Quran.Displayer
             }
 
             // Measure and pre-arrange so that the size is known from the initialization
-            Measure(new Size(650, double.PositiveInfinity));
+            Measure(new System.Windows.Size(650, double.PositiveInfinity));
             Arrange(new Rect(0, 0, 650, DesiredSize.Height));
+
+            //HighlightWord(0);
         }
+
+        #region Karaoke
+        #region Utils
+        private (int, int) WordIndexToCharIndex(int index)
+        {
+            int charIndex = 0;
+            int wordIndex = 0;
+
+            foreach (string word in ArabicRTB_Run.Text.Split(' '))
+            {
+                Console.WriteLine($"{word}: {word.Length}");
+                if (wordIndex == index)
+                {
+                    return (charIndex, charIndex + word.Length);
+                }
+
+                wordIndex++;
+                charIndex += word.Length + 3;
+            }
+
+            return (0, 0);
+            //throw new IndexOutOfRangeException();
+        }
+        #endregion
+        public void HighlightWord(int index)
+        {
+            //TextPointer pointer = ArabicTB.
+            TextPointer pointer = ArabicRTB.Document.ContentStart;
+
+            (int, int) charIndex = WordIndexToCharIndex(index);
+
+            string word = ArabicRTB_Run.Text.Split(' ')[index];
+            int indexx = ArabicRTB_Run.Text.IndexOf(word, StringComparison.Ordinal);
+            int lengthh = word.Length;
+
+            TextPointer start = pointer.GetPositionAtOffset(ArabicRTB_Run.Text.IndexOf(word, StringComparison.Ordinal));
+            TextPointer end = start.GetPositionAtOffset(ArabicRTB_Run.Text.IndexOf(word, StringComparison.Ordinal) + word.Length);
+
+            try
+            {
+                var selection = new TextRange(start, end);
+                selection.ApplyPropertyValue(TextElement.ForegroundProperty, System.Windows.Media.Brushes.Goldenrod);
+            } catch
+            {
+
+            }
+        }
+
+        public void CleanHighlighting()
+        {
+
+        }
+        #endregion
     }
 }
